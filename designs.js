84f2a2ccf29b4bcaf4ccf950d43a24898a03229c
rothwulf <radio_seattle@hotmail.com>
2020-03-26 17:41:11 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function() { //The code only run when the DOM is ready.  This is detected by jQuery//
   colorCell();
-  $('#sizePicker').submit(function(event) {
-    event.preventDefault();
-    let sizeData = $(this).serializeArray();
+  $('#sizePicker').submit(function(event) { //The function executes every time that the event is triggered//
+    event.preventDefault(); //The event happens before the actual submission, so we cancel the submission calling event.preventDefault()//
+    let sizeData = $(this).serializeArray(); //sizeData variable set to the value of an array. $(this) is a query object
     sizeHeight = sizeData[0]["value"];
     sizeWidth = sizeData[1]["value"];
     $("#pixelCanvas").empty();
