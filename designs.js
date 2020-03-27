a39a85bc14365b9313738b6e7461bc57ef8a710c
rothwulf <radio_seattle@hotmail.com>
2020-03-27 09:56:17 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,11 +1,13 @@
 $(document).ready(function() { //The code only run when the DOM is ready.  This is detected by jQuery//
-  colorCell();
+  colorCell(); //Select color input//
   $('#sizePicker').submit(function(event) { //The function executes every time that the event is triggered//
     event.preventDefault(); //The event happens before the actual submission, so we cancel the submission calling event.preventDefault()//
-    let sizeData = $(this).serializeArray(); //sizeData variable set to the value of an array. $(this) is a query object
-    sizeHeight = sizeData[0]["value"];
-    sizeWidth = sizeData[1]["value"];
-    $("#pixelCanvas").empty();
+    let sizeData = $(this).serializeArray(); //(let) is used to declare local variables.  sizeData variable set to the value of an array.// 
+    //$(this) ends up in the part of the jQuery constructor that takes care of a supplied DOMElement.//
+    //Used to create a JavaScript array of objects that is ready to be encoded as a JSON string.//
+    sizeHeight = sizeData[0]["value"]; //Place Height in the first position of the array and Width in the second position.//
+    sizeWidth = sizeData[1]["value"];  //Place Height in the first position of the array and Width in the second position.//
+    $("#pixelCanvas").empty(); //removes all child nodes and content from the selected elements.//
     makeGrid(sizeHeight, sizeWidth);
   });
 
